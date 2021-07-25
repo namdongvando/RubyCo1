e85ee9cb6ef6373127a1caafb4b3c39bea13372c
namdongvando <namdong92@gmail.com>
2021-07-25 16:18:06 +0700

    SuaForm

--- theme/backend/backend.php
+++ theme/backend/backend.php
@@ -68,10 +68,7 @@ class backend extends \Model\Database {
                                 </li>
                             </ul>
                         </li>
-                        <!-- Control Sidebar Toggle Button -->
-                        <li>
-                            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-                        </li>
+
                     </ul>
                 </div>
             </nav>
@@ -109,8 +106,6 @@ class backend extends \Model\Database {
                             <i class="fa fa-image"></i> <span>Thư Viện Hình Ảnh</span>
                         </a>
                     </li>
-
-
                     <li class="treeview <?php echo \Model\Breadcrumb::CheckMenuAcrive("Controller_mpage") ? 'active' : '' ?> ">
                         <a href="/mpage/index">
                             <i class="fa fa-list-alt"></i> <span>Quản Lý Bài Viết</span>
@@ -133,41 +128,45 @@ class backend extends \Model\Database {
                             <li><a href="/mnews/addnews"><i class="fa fa-circle-o"></i> Thêm Bài viết</a></li>
                         </ul>
                     </li>
-                    <li class="treeview  <?php echo \Model\Breadcrumb::CheckMenuAcrive("Controller_madv") ? 'active' : '' ?>">
-                        <a href="/madv/index"><i class="fa fa-home"></i>
-                            <span>Trang Chủ</span>
-                            <i class="fa fa-angle-right pull-right"></i>
-                        </a>
-                        <ul class="treeview-menu  ">
-                            <li class="active" ><a  href="/madv/homeslide/"><i class="fa fa-circle-o"></i>Quản Lý Banner Động</a></li>
-                            <li class="active" ><a  href="/madv/doitac"><i class="fa fa-circle-o"></i>Đối Tác</a></li>
-                            <li class="active" ><a  href="/madv/khachhang/"><i class="fa fa-circle-o"></i>Ý Kiến Khách Hàng </a></li>
-                            <li class="active" ><a  href="/mtheme/mhome/home"><i class="fa fa-circle-o"></i>Giao Diện</a></li>
-                        </ul>
-                    </li>
-                    <li class="treeview hidden">
-                        <a href="/madv/index"><i class="fa fa-circle-o"></i> Quản Lý Quảng Cáo</a>
-                        <ul class="treeview-menu hidden">
-                            <li><a href="/madv/index"><i class="fa fa-circle-o"></i> Quản Lý Quảng Cáo</a></li>
-                            <li class="hidden"  ><a href="/madv/video"><i class="fa fa-circle-o"></i> Quản Lý Video</a></li>
-                        </ul>
-                    </li>
-                    <li class="treeview">
-                        <a href="/mtheme/thememenu/home/TopMainMenu">
-                            <i class="fa fa-list-alt"></i> <span>Quản Lý Menu</span>
-                            <i class="fa fa-angle-right pull-right"></i>
-                        </a>
-
-                    </li>
-                    <li class="treeview ">
-                        <a href="/minfor/index/thongtincongty">
-                            <i class="fa fa-list-alt"></i> <span>Thông tin công ty</span>
-                            <i class="fa fa-angle-right pull-right"></i>
-                        </a>
-                    </li>
                     <?php
-                    $Duser = \Module\duser\Model\Duser::CurentUsernameAdmin(true);
-                    if ($Duser->Groups == \Module\duser\Model\Duser::$CodeAdmin) {
+                    if (\Module\duser\Model\Duser::CheckQuyen([\Module\duser\Model\Duser::CodeSuperAdmin, \Module\duser\Model\Duser::$CodeAdmin])) {
+                        ?>
+                        <li class="treeview  <?php echo \Model\Breadcrumb::CheckMenuAcrive("Controller_madv") ? 'active' : '' ?>">
+                            <a href="/madv/index"><i class="fa fa-home"></i>
+                                <span>Trang Chủ</span>
+                                <i class="fa fa-angle-right pull-right"></i>
+                            </a>
+                            <ul class="treeview-menu  ">
+                                <li class="active" ><a  href="/madv/homeslide/"><i class="fa fa-circle-o"></i>Quản Lý Banner Động</a></li>
+                                <li class="active" ><a  href="/madv/doitac"><i class="fa fa-circle-o"></i>Đối Tác</a></li>
+                                <li class="active" ><a  href="/madv/khachhang/"><i class="fa fa-circle-o"></i>Ý Kiến Khách Hàng </a></li>
+                                <li class="active" ><a  href="/mtheme/mhome/home"><i class="fa fa-circle-o"></i>Giao Diện</a></li>
+                            </ul>
+                        </li>
+                        <?php
+                    }
+                    if (\Module\duser\Model\Duser::CheckQuyen([\Module\duser\Model\Duser::CodeSuperAdmin, \Module\duser\Model\Duser::$CodeAdmin])) {
+                        ?>
+                        <li class="treeview">
+                            <a href="/mtheme/thememenu/home/TopMainMenu">
+                                <i class="fa fa-list-alt"></i> <span>Quản Lý Menu</span>
+                                <i class="fa fa-angle-right pull-right"></i>
+                            </a>
+
+                        </li>
+                        <?php
+                    }
+                    if (\Module\duser\Model\Duser::CheckQuyen([\Module\duser\Model\Duser::CodeSuperAdmin, \Module\duser\Model\Duser::$CodeAdmin])) {
+                        ?>
+                        <li class="treeview ">
+                            <a href="/minfor/index/thongtincongty">
+                                <i class="fa fa-list-alt"></i> <span>Thông tin công ty</span>
+                                <i class="fa fa-angle-right pull-right"></i>
+                            </a>
+                        </li>
+                        <?php
+                    }
+                    if (\Module\duser\Model\Duser::CheckQuyen([\Module\duser\Model\Duser::CodeSuperAdmin, \Module\duser\Model\Duser::$CodeAdmin])) {
                         ?>
                         <li class="  treeview  ">
                             <a href="/duser/index/index">
@@ -178,11 +177,7 @@ class backend extends \Model\Database {
                         <?php
                     }
                     ?>
-
                 </ul>
-                <div class="clearfix" ></div>
-                <div class="hidden connectedSortable" style="background-color: #ddd;" >
-                </div>
             </section>
             <!-- /.sidebar -->
         </aside>
@@ -229,7 +224,7 @@ class backend extends \Model\Database {
         <script src="/public/admin/dist/js/app.min.js"></script>
         <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
         <script src="/public/admin/dist/js/demo.js"></script>
-        <script src="/public/admin/Javascript.js" type="text/javascript"></script>
+        <script src="/public/admin/Javascript.js?<?php echo filemtime('public/admin/Javascript.js'); ?>" type="text/javascript"></script>
         <script>
 
                             $(function() {
@@ -325,6 +320,7 @@ class backend extends \Model\Database {
 
 
         </script>
+        <script src="https://www.google.com/recaptcha/api.js?render=6LdxarcbAAAAAGyiHFhosVhbdNa5uK-ufiERSCF3"></script>
         <?php
     }
 
@@ -333,7 +329,8 @@ class backend extends \Model\Database {
         $brea->backend();
     }
 
-    public function Footer() {
+    public
+            function Footer() {
         ?>
         <footer class="main-footer">
             <div class="pull-right hidden-xs">
@@ -341,172 +338,6 @@ class backend extends \Model\Database {
             </div>
             <strong>Copyright &copy; <?php echo date("Y", time()); ?> <a href="https://nguyenvando.net">nguyenvando.net</a>.</strong>
         </footer>
-
-        <!-- Control Sidebar -->
-        <aside class="control-sidebar control-sidebar-dark">
-            <!-- Create the tabs -->
-            <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-                <li><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-
-                <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-            </ul>
-            <!-- Tab panes -->
-            <div class="tab-content">
-                <!-- Home tab content -->
-                <div class="tab-pane" id="control-sidebar-home-tab">
-                    <h3 class="control-sidebar-heading">Recent Activity</h3>
-                    <ul class="control-sidebar-menu">
-                        <li>
-                            <a href="javascript::;">
-                                <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-                                <div class="menu-info">
-                                    <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-                                    <p>Will be 23 on April 24th</p>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javascript::;">
-                                <i class="menu-icon fa fa-user bg-yellow"></i>
-                                <div class="menu-info">
-                                    <h4 class="control-sidebar-subheading">Frodo Updated His Profile</h4>
-                                    <p>New phone +1(800)555-1234</p>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javascript::;">
-                                <i class="menu-icon fa fa-envelope-o bg-light-blue"></i>
-                                <div class="menu-info">
-                                    <h4 class="control-sidebar-subheading">Nora Joined Mailing List</h4>
-                                    <p>nora@example.com</p>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javascript::;">
-                                <i class="menu-icon fa fa-file-code-o bg-green"></i>
-                                <div class="menu-info">
-                                    <h4 class="control-sidebar-subheading">Cron Job 254 Executed</h4>
-                                    <p>Execution time 5 seconds</p>
-                                </div>
-                            </a>
-                        </li>
-                    </ul><!-- /.control-sidebar-menu -->
-
-                    <h3 class="control-sidebar-heading">Tasks Progress</h3>
-                    <ul class="control-sidebar-menu">
-                        <li>
-                            <a href="javascript::;">
-                                <h4 class="control-sidebar-subheading">
-                                    Custom Template Design
-                                    <span class="label label-danger pull-right">70%</span>
-                                </h4>
-                                <div class="progress progress-xxs">
-                                    <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javascript::;">
-                                <h4 class="control-sidebar-subheading">
-                                    Update Resume
-                                    <span class="label label-success pull-right">95%</span>
-                                </h4>
-                                <div class="progress progress-xxs">
-                                    <div class="progress-bar progress-bar-success" style="width: 95%"></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javascript::;">
-                                <h4 class="control-sidebar-subheading">
-                                    Laravel Integration
-                                    <span class="label label-warning pull-right">50%</span>
-                                </h4>
-                                <div class="progress progress-xxs">
-                                    <div class="progress-bar progress-bar-warning" style="width: 50%"></div>
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="javascript::;">
-                                <h4 class="control-sidebar-subheading">
-                                    Back End Framework
-                                    <span class="label label-primary pull-right">68%</span>
-                                </h4>
-                                <div class="progress progress-xxs">
-                                    <div class="progress-bar progress-bar-primary" style="width: 68%"></div>
-                                </div>
-                            </a>
-                        </li>
-                    </ul><!-- /.control-sidebar-menu -->
-
-                </div><!-- /.tab-pane -->
-                <!-- Stats tab content -->
-                <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div><!-- /.tab-pane -->
-                <!-- Settings tab content -->
-                <div class="tab-pane" id="control-sidebar-settings-tab">
-                    <form method="post">
-                        <h3 class="control-sidebar-heading">General Settings</h3>
-                        <div class="form-group">
-                            <label class="control-sidebar-subheading">
-                                Report panel usage
-                                <input type="checkbox" class="pull-right" checked>
-                            </label>
-                            <p>
-                                Some information about this general settings option
-                            </p>
-                        </div><!-- /.form-group -->
-
-                        <div class="form-group">
-                            <label class="control-sidebar-subheading">
-                                Allow mail redirect
-                                <input type="checkbox" class="pull-right" checked>
-                            </label>
-                            <p>
-                                Other sets of options are available
-                            </p>
-                        </div><!-- /.form-group -->
-
-                        <div class="form-group">
-                            <label class="control-sidebar-subheading">
-                                Expose author name in posts
-                                <input type="checkbox" class="pull-right" checked>
-                            </label>
-                            <p>
-                                Allow the user to show his name in blog posts
-                            </p>
-                        </div><!-- /.form-group -->
-
-                        <h3 class="control-sidebar-heading">Chat Settings</h3>
-
-                        <div class="form-group">
-                            <label class="control-sidebar-subheading">
-                                Show me as online
-                                <input type="checkbox" class="pull-right" checked>
-                            </label>
-                        </div><!-- /.form-group -->
-
-                        <div class="form-group">
-                            <label class="control-sidebar-subheading">
-                                Turn off notifications
-                                <input type="checkbox" class="pull-right">
-                            </label>
-                        </div><!-- /.form-group -->
-
-                        <div class="form-group">
-                            <label class="control-sidebar-subheading">
-                                Delete chat history
-                                <a href="javascript::;" class="text-red pull-right"><i class="fa fa-trash-o"></i></a>
-                            </label>
-                        </div><!-- /.form-group -->
-                    </form>
-                </div><!-- /.tab-pane -->
-            </div>
-        </aside><!-- /.control-sidebar -->
-        <!-- Add the sidebar's background. This div must be placed
-             immediately after the control sidebar -->
         <div class="control-sidebar-bg"></div>
 
         <?php
